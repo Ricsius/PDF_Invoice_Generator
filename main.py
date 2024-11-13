f158c22f273d449a07a04ce6fa7a5f84f6fc8b2a
Ricsius <hajnal.ricsi@gmail.com>
2024-11-13 16:47:16 +0100

    Add header, total price of the order, company and logo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fpdf import FPDF
 from pathlib import Path
 
 DATA_SOURCE = "invoices"
+LOGO_PATH = "pythonhow.png"
 OUTPUT_PATH = "PDFs"
 PRODUCT_ID_KEY = "product_id"
 PRODUCT_NAME_KEY = "product_name"
@@ -54,5 +55,25 @@ for path in invoice_paths:
         pdf.cell(w=30, h=8, txt=str(row[AMOUNT_PURCHASED_KEY]), border=1)
         pdf.cell(w=30, h=8, txt=str(row[PRICE_PER_UNIT_KEY]), border=1)
         pdf.cell(w=30, h=8, txt=str(row[TOTAL_PRICE_KEY]), border=1, ln=1)
+
+    # Total price
+    total_price = data[TOTAL_PRICE_KEY].sum()
+
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_price), border=1, ln=1)
+
+    # Total price sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_price}", ln=1)
+    
+    # Company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt=f"PythonHow")
+    pdf.image(LOGO_PATH, w=10)
 
     pdf.output(f"{OUTPUT_PATH}/{file_name}.pdf")
